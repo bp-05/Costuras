077ecab49fa065b5753afc16b7e698657684b1c9
bp05 <bastian.parada05@inacapmail.cl>
2023-11-16 23:39:00 -0300

    primer commit app base

---
+++ app/src/main/java/cl/ingsoftware/costuras/Registro.kt
@@ -0,0 +1,59 @@
+package cl.ingsoftware.costuras
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class Registro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+
+        lateinit var auth: FirebaseAuth
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+        setup(auth)
+
+    }
+    private fun setup(auth: FirebaseAuth) {
+        val boton_registro = findViewById<View>(R.id.boton_registro) as Button
+        val email = findViewById<View>(R.id.etEmail) as EditText
+        val clave = findViewById<View>(R.id.etPass) as EditText
+        val claverep = findViewById<View>(R.id.etRPass) as EditText
+        boton_registro.setOnClickListener {
+            if (email.text.isNotEmpty() && clave.text.isNotEmpty() && claverep.text.isNotEmpty()){
+                if (clave.text.toString() == claverep.text.toString()){
+                    auth.createUserWithEmailAndPassword(email.text.toString(), clave.text.toString()).addOnCompleteListener(this){
+                        if (it.isSuccessful){
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }else{
+                            showAlert()
+                        }
+                    }
+                }
+            }
+        }
+    }
+    private fun showAlert() {
+        val alertbuilder = AlertDialog.Builder(this@Registro)
+        alertbuilder.setTitle("Error")
+        alertbuilder.setMessage("Error al autenticar usuario")
+        alertbuilder.setPositiveButton(
+            "OK"
+        ) { dialogInterface, i -> finish() }
+        val dialog = alertbuilder.show()
+        dialog.setCancelable(false)
+        dialog.setCanceledOnTouchOutside(false)
+    }
+}
+
+//a
---
+++ app/src/main/java/cl/ingsoftware/costuras/Splash.kt
@@ -0,0 +1,73 @@
+package cl.ingsoftware.costuras
+
+import android.content.Context
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val currentUser = Firebase.auth.currentUser
+        val intentLogin = Intent(this, InicioSesion::class.java)
+        val intentMain = Intent(this, MainActivity::class.java)
+        Handler(Looper.getMainLooper()).postDelayed({
+            if (isOnline(this)){
+                if (currentUser != null) {
+                    Toast.makeText(this, "si se ha accedido, con: "+currentUser.email.toString(), Toast.LENGTH_SHORT).show()
+                    startActivity(intentMain)
+                    finish()
+                    //reload()
+                }else{
+                    Toast.makeText(this, "Debes inciar sesion o registrarte", Toast.LENGTH_SHORT).show()
+                    startActivity(intentLogin)
+                    finish()
+                }
+            }else{
+                Toast.makeText(this@Splash, "No hay conexion a internet", Toast.LENGTH_SHORT).show()
+                val alertbuilder = AlertDialog.Builder(this@Splash)
+                alertbuilder.setTitle("No hay internet")
+                alertbuilder.setMessage("Por favor, activa el internet")
+                alertbuilder.setPositiveButton(
+                    "OK"
+                ) { dialogInterface, i -> finish() }
+                val dialog = alertbuilder.show()
+                dialog.setCancelable(false)
+                dialog.setCanceledOnTouchOutside(false)
+            }
+        }, 2000)
+    }
+
+    fun isOnline(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (connectivityManager != null) {
+            val capabilities =
+                connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+            if (capabilities != null) {
+                if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
+                    return true
+                } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
+                    return true
+                } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
+                    return true
+                }
+            }
+        }
+        return false
+    }
+}
